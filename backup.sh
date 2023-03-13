7ebe39599f3deb9f4870e10da98e4afa3e1a6c94
Thomas Cort <linuxgeek@gmail.com>
2023-03-13 18:57:23 -0400

    feat(backup): silence curl, print backup filename

--- backup.sh
+++ backup.sh
@@ -8,8 +8,8 @@ BACKUP_DIR=backups
 mkdir ${DIR}
 git archive --format=tar HEAD > ${DIR}/code-${NOW}.tar
 cd ${DIR}
-curl -o "logbook.adi" http://localhost:3000/logs?fmt=adi
-curl -o "logbook.json" http://localhost:3000/logs?fmt=json
+curl --silent -o "logbook.adi" http://localhost:3000/logs?fmt=adi
+curl --silent -o "logbook.json" http://localhost:3000/logs?fmt=json
 cp ../logbook.sqlite3 .
 cd ..
 
@@ -19,3 +19,5 @@ rm -rf ${DIR}
 
 mkdir -p ${BACKUP_DIR}
 mv ${ARC}.gz ${BACKUP_DIR}
+
+echo ${BACKUP_DIR}/${ARC}.gz
