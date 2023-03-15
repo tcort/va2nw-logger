50e7126733d6d054044eff9d4d1e84789a330260
Thomas Cort <linuxgeek@gmail.com>
2023-03-15 17:59:35 -0400

    refactor(db): compute stats with SQL

--- public/js/logs.js
+++ public/js/logs.js
@@ -61,10 +61,5 @@ $(function () {
 
     updateTimestamp();
 
-    $('.toggle-visibility').on('click', function () {
-        const target = $(this).data('target');
-        $(`#${target}`).toggleClass('hidden');
-    });
-
 });
 
