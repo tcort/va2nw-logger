9063d531ef263feb0f3893aacc78627346f3b26f
Thomas Cort <linuxgeek@gmail.com>
2023-03-22 16:53:28 -0400

    feat(index): announce URL

--- index.js
+++ index.js
@@ -26,5 +26,5 @@ app.use(require('./lib/routes'));
 
 /* launch the server */
 server.listen(3000, function () {
-    console.log('service up');
+    console.log('service up: http://localhost:3000/');
 });
