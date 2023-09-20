144f30d091dfd7d3290ac93334dfd5938abca38d
Thomas Cort <thomasc@ssicanada.com>
2023-09-20 02:43:55 -0400

    refactor(paper): simplified version like paper log

---
+++ views/backup.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+NOW=`date +%Y%m%d%H%M%S`
+DIR="backup-${NOW}"
+ARC="${DIR}.tar"
+BACKUP_DIR=backups
+
+mkdir ${DIR}
+git archive --format=tar HEAD > ${DIR}/code-${NOW}.tar
+cd ${DIR}
+curl --silent -o "logbook.adi"  'http://localhost:3000/logs?fmt=adi&limit=1000000&since=1900-01-01T00:00:00Z'
+curl --silent -o "logbook.cab"  'http://localhost:3000/logs?fmt=cab&limit=1000000&since=1900-01-01T00:00:00Z'
+curl --silent -o "logbook.csv"  'http://localhost:3000/logs?fmt=csv&limit=1000000&since=1900-01-01T00:00:00Z'
+curl --silent -o "logbook.json" 'http://localhost:3000/logs?fmt=json&limit=1000000&since=1900-01-01T00:00:00Z'
+cp ../logbook.sqlite3 .
+cd ..
+
+tar -cf ${ARC} ${DIR}
+gzip ${ARC}
+rm -rf ${DIR}
+
+mkdir -p ${BACKUP_DIR}
+mv ${ARC}.gz ${BACKUP_DIR}
+
+echo ${BACKUP_DIR}/${ARC}.gz
