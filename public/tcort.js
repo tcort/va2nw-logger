35a91182cf7ecab74c8c8e8ec9755557ee21c445
Thomas Cort <linuxgeek@gmail.com>
2023-09-28 04:41:29 -0400

    chore: update tcort.css

--- public/tcort.js
+++ public/tcort.js
@@ -1,3 +1,23 @@
+/*
+    Copyright (c) 2023 Thomas Cort <linuxgeek@gmail.com>
+
+    Permission to use, copy, modify, and distribute this software for any
+    purpose with or without fee is hereby granted, provided that the above
+    copyright notice and this permission notice appear in all copies.
+
+    THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+    WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+    MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+    ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+    WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+    ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+    OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+/*
+    Version 2023.09.22a
+ */
+
 $(function () {
     $('.navtab').on('click', function () {
         $('.navtab').each(function () {
@@ -28,6 +48,13 @@ $(function () {
                 $(inputs[thisInputIndex + 1]).focus().select();
             }
         }
+    });
+
+    $('.toggle-visibility').on('click', function () {
+        const target = $(this).data('target');
+        $(`#${target}`).toggleClass('hidden');
+
+        $(this).text( $(this).text() === '-' ? '+' : '-' );
     });
 
     $('select[data-autoselect]').each(function () {
@@ -71,4 +98,14 @@ function tctypeahead(input, datalist, fn) {
         });
     });
 
+}
+
+function completer(baseUrl, toUpper = false) {
+    return function (input, callback) {
+        const startsWith = toUpper ? `${input}`.toUpperCase() : `${input}`;
+        fetch(baseUrl + encodeURIComponent(startsWith))
+            .then((response) => response.json())
+            .then(json => callback(null, json))
+            .catch(err => callback(err));
+    };
 }
